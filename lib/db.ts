d277e83ef7a1ba9c61fa3285774b2d0bcbb7ae94
Muntasirul-2002 <muntasirul.msd@gmail.com>
2025-08-30 19:14:32 +0530

    fix mysql issue; deploy on cloud

--- lib/db.ts
+++ lib/db.ts
@@ -5,15 +5,14 @@ export const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 5,
-    queueLimit: 0,
-    connectTimeout: 10000,
-    idleTimeout: 30000,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-    port : Number(process.env.DB_PORT) || 3306
+    port : Number(process.env.DB_PORT) || 3306,
+    ssl:{
+        rejectUnauthorized: false
+    },
+    charset: 'utf8mb4'
 })
+
+
 
 
 export async function testConnection() {
